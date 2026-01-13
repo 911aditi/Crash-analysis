509bf7dc41dcc3056edcaa83c61b6dd298d629c4
Aditi <imaditi.bhatnagar@gmail.com>
2026-01-13 13:12:53 +0530

    Update crash_home.py

--- Car_Dashboard/crash_home.py
+++ Car_Dashboard/crash_home.py
@@ -3,5 +3,6 @@ import streamlit as st
 st.markdown("<h1 style='color:white; background-color:black; text-align: center; border:2 px dashed black;'>Car crash Analysis Dashboard</h1>", unsafe_allow_html=True)
 # unsafe_allow_html = True to allow html tags in streamlit
 
-st.markdown("<p style='color:brown; text-align: center;'>Explore the insights of the Car crashes dataset </p>", unsafe_allow_html=True)
+st.markdown("<p style='color:white; text-align: center;'>Explore the insights of the Car crashes dataset </p>", unsafe_allow_html=True)
+
 st.image("https://t4.ftcdn.net/jpg/04/86/66/75/360_F_486667590_RXONs4sn4K7OO7rtGYhN8aH3UnseCjTu.jpg", width=700)
